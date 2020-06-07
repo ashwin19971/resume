a5b5135ad82b09dee4a64dea19394e4ca9caa650
ashwiniimjobs <ashwin@iimjobs.com>
2020-06-07 15:47:03 +0530

    Initial commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.scss';
+import Routes from '../../routes';
+import Header from '../../components/Header';
+
+const App = () => (
+  <div>
+    <Header />
+    <Routes />
+  </div>
+)
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import { BASE_ROUTE } from '../models/routes';
+import HomePage from '../modules/HomePage/pages/HomePage';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path={BASE_ROUTE} exact component={HomePage} />
+        <Route component={() => console.log("Page not found")} />
+        <Redirect from="//*" to="/*" />
+      </Switch>
+    )
+  }
+}
+
+export default Routes;
