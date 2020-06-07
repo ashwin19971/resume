bf71e81ea1866e7e87623ea586632cf2a92f26c6
ashwiniimjobs <ashwin@iimjobs.com>
2020-06-07 16:58:34 +0530

    Updates

---
+++ precache-manifest.5c19ef671431575d577bc6a9f442eefe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d197df02d3a1ead88d58304befaee0",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "ec7910cade45ac1042aa",
+    "url": "/resume/static/css/main.239d6554.chunk.css"
+  },
+  {
+    "revision": "403829d2ae2c6fb2a7ad",
+    "url": "/resume/static/js/2.aeded231.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.aeded231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7910cade45ac1042aa",
+    "url": "/resume/static/js/main.067a3474.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  },
+  {
+    "revision": "0d1b1ad5c9399c3557794cc8bf28a0f0",
+    "url": "/resume/static/media/fontello.0d1b1ad5.svg"
+  },
+  {
+    "revision": "1a11e2d4bcfe002e69d1747945271171",
+    "url": "/resume/static/media/fontello.1a11e2d4.woff"
+  },
+  {
+    "revision": "1f35aa2ddd30e4d069738c441e18beae",
+    "url": "/resume/static/media/fontello.1f35aa2d.eot"
+  },
+  {
+    "revision": "32aa482f87a8366004b280eecc74a2f2",
+    "url": "/resume/static/media/fontello.32aa482f.woff2"
+  },
+  {
+    "revision": "41da2eafacbae58ca8d287d1169f4fae",
+    "url": "/resume/static/media/fontello.41da2eaf.ttf"
+  }
+]);
